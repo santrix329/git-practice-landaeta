1f7b7dcd4d14b1bb8ecb1f0d52c0e18123871364
santrix329 <tiagoalexey17@gmail.com>
2026-01-26 02:27:28 -0400

    feat: asegurar subida de calculadora

---
+++ calculadora.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int a, int b) {
+    return a + b;
+}
+// Finalizado
+int main() {
+    cout << "5 + 3 = " << sumar(5, 3) << endl;
+    return 0;
+}
